6f08600c209b3961c5b6ca8922d641ad2ca989ef
David <david.breier@outlook.de>
2018-10-17 10:17:37 +0200

    bootstrap and navbar added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import Contact from "./components/Contact";
 import Header from "./components/Header";
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,21 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { setMaxListeners } from "cluster";
+import { SSL_OP_SSLEAY_080_CLIENT_DH_BUG } from "constants";
 
 const Header = props => {
   const { branding } = props;
   return (
+    <nav className = "navbar navbar-expand-sm navbar-dark bg-danger mb-3 py-0 >
+   <div className="container">
+   <a href="/" className="navbar-brand">{branding}</a>
+   </div> 
     <div>
-      <h1>{ branding }</h1>
-    </div>
+      <ul className="navbar-nav mr-auto">
+       <li className="nav-item"><a href="/" className="nav-link">Home</a></li>
+           </ul>
+   </div>
+   </nav>
   );
 };
 
